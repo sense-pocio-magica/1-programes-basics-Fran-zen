899e45f40835ec51d8310962db055de9fadf6a71
Fran <181867666+Fran-zen@users.noreply.github.com>
2025-09-30 16:45:04 +0200

    Doing Ex12

--- exercicis/exercici12/Program.cs
+++ exercicis/exercici12/Program.cs
@@ -27,6 +27,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Primer numero");
+        var n1 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Primer segon");
+        var n2 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine($"{n1} + {n2} = {n1 + n2}");
+        Console.WriteLine($"{n1} - {n2} = {n1 - n2}");
+        Console.WriteLine($"{n1} * {n2} = {n1 * n2}");
+        Console.WriteLine($"{n1} / {n2} = {n1 / n2}");
     }
 }
