ef36d010dce168213aef81d119fa0e500227008f
Fran <181867666+Fran-zen@users.noreply.github.com>
2025-09-30 16:11:07 +0200

    Done Ex4

--- exercicis/exercici4/Program.cs
+++ exercicis/exercici4/Program.cs
@@ -10,7 +10,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        
+        Console.WriteLine("Quant a costat?");
+        var euro = ConvertToInt16(Console.ReadLine());
+        double pesetas = 166.386;
+        Console.WriteLine($"{euro} = {euros * pesetas}");
     }
 }
